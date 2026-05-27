5c96ac5ebcb0f4091ab96a239b212a70398979f8
Michał <michalmarton01@gmail.com>
2026-05-27 08:18:54 +0200

    LED_TASK

--- Src/main.c
+++ Src/main.c
@@ -1,20 +1,77 @@
 #include "stm32f411xe.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "gpio/gpio.h"
+#include "clock/clock.h"
 
-static void vLedTask(void *pv) {
+#define LED_GREEN   12  // PD12
+#define LED_ORANGE  13  // PD13
+#define LED_RED     14  // PD14
+#define LED_BLUE    15  // PD15
+
+static void vSequenceTask(void *pv) {
     (void)pv;
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-    GPIOD->MODER |= (1 << (12 * 2));
+
+    // Inicjalizacja GPIO
+    GPIO_EnableClock(GPIOD);
+    GPIO_ConfigPin(GPIOD, LED_GREEN,  GPIO_MODE_OUTPUT, GPIO_PULL_NONE, GPIO_OTYPE_PP);
+    GPIO_ConfigPin(GPIOD, LED_ORANGE, GPIO_MODE_OUTPUT, GPIO_PULL_NONE, GPIO_OTYPE_PP);
+    GPIO_ConfigPin(GPIOD, LED_RED,    GPIO_MODE_OUTPUT, GPIO_PULL_NONE, GPIO_OTYPE_PP);
+    GPIO_ConfigPin(GPIOD, LED_BLUE,   GPIO_MODE_OUTPUT, GPIO_PULL_NONE, GPIO_OTYPE_PP);
+
+    // Wszystkie zgaszone na start
+    GPIO_WritePin(GPIOD, LED_GREEN,  GPIO_PIN_RESET);
+    GPIO_WritePin(GPIOD, LED_ORANGE, GPIO_PIN_RESET);
+    GPIO_WritePin(GPIOD, LED_RED,    GPIO_PIN_RESET);
+    GPIO_WritePin(GPIOD, LED_BLUE,   GPIO_PIN_RESET);
 
     for (;;) {
-        GPIOD->ODR ^= (1 << 12);
-        vTaskDelay(pdMS_TO_TICKS(500));
+        // 3 okrążenia
+        for (int lap = 0; lap < 3; lap++) {
+            // Zapalanie po kolei
+            GPIO_WritePin(GPIOD, LED_GREEN,  GPIO_PIN_SET);
+            vTaskDelay(pdMS_TO_TICKS(80));
+            GPIO_WritePin(GPIOD, LED_ORANGE, GPIO_PIN_SET);
+            vTaskDelay(pdMS_TO_TICKS(80));
+            GPIO_WritePin(GPIOD, LED_RED,    GPIO_PIN_SET);
+            vTaskDelay(pdMS_TO_TICKS(80));
+            GPIO_WritePin(GPIOD, LED_BLUE,   GPIO_PIN_SET);
+            vTaskDelay(pdMS_TO_TICKS(80));
+
+            // Gaszenie po kolei w tym samym tempie
+            GPIO_WritePin(GPIOD, LED_GREEN,  GPIO_PIN_RESET);
+            vTaskDelay(pdMS_TO_TICKS(80));
+            GPIO_WritePin(GPIOD, LED_ORANGE, GPIO_PIN_RESET);
+            vTaskDelay(pdMS_TO_TICKS(80));
+            GPIO_WritePin(GPIOD, LED_RED,    GPIO_PIN_RESET);
+            vTaskDelay(pdMS_TO_TICKS(80));
+            GPIO_WritePin(GPIOD, LED_BLUE,   GPIO_PIN_RESET);
+            vTaskDelay(pdMS_TO_TICKS(80));
+        }
+
+        // 3x mrugnięcie wszystkich jednocześnie
+        for (int i = 0; i < 3; i++) {
+            GPIO_WritePin(GPIOD, LED_GREEN,  GPIO_PIN_SET);
+            GPIO_WritePin(GPIOD, LED_ORANGE, GPIO_PIN_SET);
+            GPIO_WritePin(GPIOD, LED_RED,    GPIO_PIN_SET);
+            GPIO_WritePin(GPIOD, LED_BLUE,   GPIO_PIN_SET);
+            vTaskDelay(pdMS_TO_TICKS(120));
+
+            GPIO_WritePin(GPIOD, LED_GREEN,  GPIO_PIN_RESET);
+            GPIO_WritePin(GPIOD, LED_ORANGE, GPIO_PIN_RESET);
+            GPIO_WritePin(GPIOD, LED_RED,    GPIO_PIN_RESET);
+            GPIO_WritePin(GPIOD, LED_BLUE,   GPIO_PIN_RESET);
+            vTaskDelay(pdMS_TO_TICKS(120));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
 
 int main(void) {
-    xTaskCreate(vLedTask, "LED", 128, NULL, 1, NULL);
+    SystemClock_Config();
+
+    xTaskCreate(vSequenceTask, "SEQ", 256, NULL, 1, NULL);
     vTaskStartScheduler();
     for (;;) {}
 }
